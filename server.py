7b8c5be9fa9bb5ffc410abce774ca1bb032fc3d1
BNNJ <BNNJ1974@gmail.com>
2022-06-17 14:43:08 +0200

    reworked club handling

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
+# from dataclasses import dataclass
 
 from utils import (
 	load_clubs,
@@ -17,55 +18,69 @@ competitions_file = "competitions.json"
 competitions = load_competitions(competitions_file)
 clubs = load_clubs(clubs_file)
 
+# @dataclass
+# class Club:
+# 	email: str
+# 	name: str
+# 	points: int
+
+club = None
+
 @app.route('/')
 def index():
 	return render_template('index.html')
 
 
-@app.route('/showSummary', methods=['POST'])
-def show_summary():
+@app.route('/login', methods=['POST'])
+def login():
+	global club 
 	club = get_item(clubs, lambda c: c['email'] == request.form['email'])
 	if club is None:
 		flash("No club with this email was found")
 		return redirect(url_for('index'))
 	else:
-		return render_template('welcome.html', club=club, competitions=competitions)
+		return redirect(url_for('show_summary'))
 
 
-@app.route('/book/<competition>/<club>')
-def book(competition, club):
-	found_club = get_item(clubs, lambda c: c['name'] == club)
-	found_competition = get_item(competitions, lambda c: c['name'] == competition)
+@app.route('/showSummary')
+def show_summary():
+	return render_template('welcome.html', club=club, competitions=competitions)
 
-	if found_club is None or found_competition is None:
+
+@app.route('/book/<competition>')
+def book(competition):
+	competition = get_item(competitions, lambda c: c['name'] == competition)
+
+	if club is None or competition is None:
 		flash("Something went wrong-please try again")
-		return render_template('welcome.html', club=club, competitions=competitions)
-	elif date_is_past(found_competition['date']):
+		return redirect(url_for('show_summary'))
+		# return render_template('welcome.html', club=club, competitions=competitions)
+	elif date_is_past(competition['date']):
 		flash("This competition has already taken place")
-		return render_template('welcome.html', club=club, competitions=competitions)
+		return redirect(url_for('show_summary'))
+		# return render_template('welcome.html', club=club, competitions=competitions)
 	else:
-		return render_template('booking.html', club=found_club, competition=found_competition)
+		return render_template('booking.html', club=club, competition=competition)
 
 
 @app.route('/purchasePlaces', methods=['POST'])
 def purchase_places():
 	competition = get_item(competitions, lambda c: c['name'] == request.form['competition'])
-	club = get_item(clubs, lambda c: c['name'] == request.form['club'])
+	# club = get_item(clubs, lambda c: c['name'] == request.form['club'])
 
 	places_requested = int(request.form['places'])
 	if err := purchase_error(places_requested, club['points']):
 		flash(err)
-		return render_template("booking.html", club=club, competition=competition)
+		return redirect(url_for('book', competition=competition))
+		# return render_template("booking.html", club=club, competition=competition)
 	else:
 		competition['numberOfPlaces'] -= places_requested
 		club['points'] -= places_requested
-		# save_clubs(clubs)
-		# save_competitions(competitions)
 		flash("Great-booking complete!")
-		return render_template('welcome.html', club=club, competitions=competitions)
+		return redirect(url_for('show_summary'))
+		# return render_template('welcome.html', club=club, competitions=competitions)
 
 
-# TODO: Add route for points display
 @app.route('/displayBoard')
 def display_board():
 	return render_template('display_board.html', clubs=clubs)
